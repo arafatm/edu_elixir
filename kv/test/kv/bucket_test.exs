b7eae153ecc78fc5facafc20c4c9797bad05ad39
Arafat M <github@arafatm.com>
2017-07-12 11:37:47 -0700

    Agent delete key

--- kv/test/kv/bucket_test.exs
+++ kv/test/kv/bucket_test.exs
@@ -12,4 +12,12 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "deletes value by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+
+    assert KV.Bucket.get(bucket, "milk") == 3
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
