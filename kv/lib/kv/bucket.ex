4cd2750d916216817332608c8de1b033324bbca6
Arafat M <github@arafatm.com>
2017-07-12 12:25:27 -0700

    remove debug messages

--- kv/lib/kv/bucket.ex
+++ kv/lib/kv/bucket.ex
@@ -27,11 +27,7 @@ defmodule KV.Bucket do
   Returns the current value of `key`, if `key` exists.
   """
   def delete(bucket, key) do
-    IO.puts "client: #{inspect Process.info(self())[:links]}"
-    Process.sleep(1000)
     Agent.get_and_update(bucket, fn dict ->
-      IO.puts "server: #{inspect Process.info(self())[:links]}"
-      Process.sleep(1000)
       Map.pop(dict, key)
     end)
   end
