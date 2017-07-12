3574e2220482131d72897ffa23514cdad6c26f9f
Arafat M <github@arafatm.com>
2017-07-11 21:47:16 -0700

    debug message

--- kv/lib/kv.ex
+++ kv/lib/kv.ex
@@ -4,11 +4,14 @@ defmodule KV do
   end
 
   defp loop(map) do
+    IO.puts "loop #{inspect map}"
     receive do
       {:get, key, caller} ->
+        IO.puts ":get #{inspect key}, #{inspect caller}" 
         send caller, Map.get(map, key)
         loop(map)
       {:put, key, value} ->
+        IO.puts ":put #{inspect key}, #{inspect value}" 
         loop(Map.put(map, key, value))
     end
   end
