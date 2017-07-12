c79d28e297a82a71fbf3a6d51df149f735796858
Arafat M <github@arafatm.com>
2017-07-12 11:27:22 -0700

    test watcher

--- kv/mix.exs
+++ kv/mix.exs
@@ -28,6 +28,6 @@ defmodule KV.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:mix_test_watch, "~> 0.3", only: :dev, runtime: false}]
   end
 end
