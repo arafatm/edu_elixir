276a2caad9453e65fe3e4f5d64a212d4a4b8a497
Arafat M <github@arafatm.com>
2017-07-17 15:18:36 -0700

    test registry crash

--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -22,4 +22,18 @@ defmodule KV.RegistryTest do
     Agent.stop(bucket)
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    ref = Process.monitor(bucket)
+    Process.exit(bucket, :shutdown)
+
+    # Wait until the bucket is dead
+    assert_receive {:DOWN, ^ref, _, _, _}
+
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
